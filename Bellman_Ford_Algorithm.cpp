5ed1e4f4cbc160b7fc3ee56848518312e4222b3d
Sahil Velhal <53365607+sahil-777@users.noreply.github.com>
2020-07-29 02:06:26 +0530

    Bellman-Ford Algorithm : SSSP for graph containing -ve weights

---
+++ Bellman_Ford_Algorithm.cpp
@@ -0,0 +1,96 @@
+//Bellman-Ford Algorithm:
+//This algorithm calculates shortest path in a graph having -ve weights.
+//This can also be used to detect -ve weight cycle.
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<iterator>
+#include<math.h>
+#include<queue>
+#include<map>
+using namespace std;
+#define SIZE 100001
+#define INF  1e9
+#define ll   long long  
+
+//Here, we are storing every edge and its weight.
+vector<int>edgelist[SIZE]; // {u,v,w} , list containg starting vertex, end vertex and weight
+int cost[SIZE];//Cost to reach a vertex from source
+int parent[SIZE];//To print path to reach a vertex 
+bool flag = false;//Most of the graphs require less than N-1 phases, (Optimisation)
+
+void bellman_ford(int s,int N){
+    cost[s]=0;// Cost to reach source vertex is zero
+    for(int i=0; i< N-1; i++){ // Outer loop will run (N-1) times
+        int j=0;
+
+        flag=false;
+        
+        while(edgelist[j].size()==3){  
+            
+            //Relaxation, if( cost[u] + w(u,v) < cost[v] ) then cost[v] = cost[u] + w(u,v).
+
+            if( cost[ edgelist[j][0] ] + edgelist[j][2] < cost[ edgelist[j][1] ] ){
+                cost[ edgelist[j][1] ] = cost[ edgelist[j][0] ]+ edgelist[j][2];
+                parent[ edgelist[j][1] ] = edgelist[j][0];
+                flag=true;
+            }
+            j++;
+        }
+
+        if(flag == false)//i.e. If relaxation did not take place atleast one time 
+            break;
+    }
+}
+
+void print_path(int desti){
+    cout<<"\nPath to reach given node : ";
+    while(desti!=-1){
+        cout<<desti<<" ";
+        desti=parent[desti];
+    }
+    cout<<"\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N,M;
+    cin>>N>>M;
+    
+    for(int i=0;i<M;i++){
+        int x,y,w;
+        cin>>x>>y>>w;
+        
+        //edgelist[i]={u,v,w}
+        
+        edgelist[i].push_back(x);
+        edgelist[i].push_back(y);
+        edgelist[i].push_back(w);
+     }
+
+     //Initial values
+    for(int i=1;i<=N;i++){
+        cost[i]=INF;
+        parent[i]=-1;
+    }
+    
+    int s;
+    cin>>s;
+    bellman_ford(s,N);//Passing number of nodes to run outer loop
+
+    for (int i = 1; i <= N; i++)
+        cout<<cost[i]<<" ";
+
+    int desti;
+    cin>>desti;
+    print_path(desti);
+    
+    return 0;
+}
+
+ 
